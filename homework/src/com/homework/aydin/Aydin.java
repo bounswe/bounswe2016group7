8ca0bfe333820f215e0b81af846470c6918b8535
The Formal Boogieman <The Formal Boogieman@EXHAUST>
2016-05-07 17:14:06 +0300

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of git@github.com:bounswe/bounswe2016group7.git
    
    Conflicts:
    	homework/.classpath

--- homework/src/com/homework/aydin/Aydin.java
+++ homework/src/com/homework/aydin/Aydin.java
@@ -2,12 +2,19 @@ package com.homework.aydin;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.homework.aydin.DBAydin;
 
 /**
  * Servlet implementation class Aydin
@@ -28,10 +35,62 @@ public class Aydin extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//request.getRequestDispatcher("Search.jsp").forward(request, response);
 		response.setContentType("text/html");
-        PrintWriter out = response.getWriter(); /**< Our writer to type html codes */
+        PrintWriter out = response.getWriter(); 
         out.println("<html>");
         out.println("Hi I'm Aydin </html>");
+        
+        DBAydin d = new DBAydin("test", "root", "");
+		Connection connection = d.getConnection(); /**< database connection variable*/
+		
+    	if (connection != null) {
+    		out.println("Connection to database successfull!</br>");
+    	} else {
+    		out.println("Failed to make connection!</br>");
+    	}
+    	out.println("</html>");
+    	
+    	out.println("<form>  Search:<br>  <input type=\"text\" name=\"s_bar\"><br> "
+    			+ "<input type = \"submit\" value = \"Submit\">"
+    			+ "</form>");
+    	out.println("");
+    	
+    	String searchTerm = request.getParameter("s_bar");
+    
+    	ResultSet rs;
+    	String userid = " ";
+    	String username = " ";
+    	ArrayList<String> ids = new ArrayList<String>();
+    	try {
+			Statement st = connection.createStatement();
+			rs = st.executeQuery("SELECT iduser,name FROM user WHERE name ='" + searchTerm + "'");
+			while (rs.next()) {
+				userid = rs.getString("IDUSER");
+				username = rs.getString("NAME");
+				ids.add(userid);
+				String checkBoxName = userid;
+				out.println("<input type=\"checkbox\" name=" + checkBoxName + "/>" + username + "<br>");
+				
+				//out.println(userid+ " "+username + "</br>");
+			}
+		} catch (SQLException e) {
+			out.print("Failure. </br>");
+			e.printStackTrace();
+			
+		}
+    	
+    	out.println("<form><input type=\"submit\" name=\"save\" value= \"Save\"/></form>");
+    	String saveResult = request.getParameter("save");
+    	
+    	for(String id: ids){
+    		if(request.getParameter(id) != null){
+    		out.println(id + "<br>");
+    		}
+    	}
+    		
+			
+
 	}
 
 	/**
@@ -39,6 +98,7 @@ public class Aydin extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
 	}
 
 }
