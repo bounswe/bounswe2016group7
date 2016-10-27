ffaf49db508ba42200e07ee84996703c91d8758a
Aydın Aksoy <aydinaksoy327@gmail.com>
2016-10-27 18:08:41 +0300

    validation for register/signup
    re-password and user type still need to be validated

--- utopic frontend/js/model/mainModel.js
+++ utopic frontend/js/model/mainModel.js
@@ -17,6 +17,31 @@ mainModel.config(function(valdrProvider) {
           'message': 'This field is required.'
         }
       }
+    },
+		'Register': {
+      'password': {
+        'size': {
+          'min': 6,
+          'max': 10,
+          'message': 'Password must be between 6 and 10 characters.'
+        },
+        'required': {
+          'message': 'Password is required.'
+        }
+      },
+			'email': {
+        'email': {
+          'message': 'Valid e-mail address is required.'
+        },
+        'required': {
+          'message': 'Email is required.'
+        }
+      },
+      'username': {
+        'required': {
+          'message': 'This field is required.'
+        }
+      }
     }
   })
 });
