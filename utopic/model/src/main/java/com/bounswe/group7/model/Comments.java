189f423ab01c2b147f72be9f3812f26f2fde30ae
myunusseker <m.yunusseker@hotmail.com>
2016-11-11 15:29:19 +0300

    Comments Model and Repository Created

---
+++ utopic/model/src/main/java/com/bounswe/group7/model/Comments.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bounswe.group7.model;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author myunu
+ */
+@Entity
+@Table(name = "COMMENTS")
+public class Comments {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "COMMENT_SEQ")
+    @SequenceGenerator(name = "COMMENT_SEQ", sequenceName = "COMMENT_SEQ", allocationSize = 1, initialValue = 1)
+    @Column(name = "comment_id", nullable = false)
+    private Long commentId;
+    
+    @Column(name = "topic_id", nullable = false)
+    private Long topicId;
+    
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+    
+    private Date dateCreated;
+    
+    private String text;
+    
+    private Double rate;
+    
+    public Comments() {
+    }
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public Long getTopicId() {
+        return topicId;
+    }
+
+    public void setTopicId(Long topicId) {
+        this.topicId = topicId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+    
+}
--- utopic/repository/src/main/java/com/bounswe/group7/repository/CommentsRepository.java
+++ utopic/repository/src/main/java/com/bounswe/group7/repository/CommentsRepository.java
@@ -5,7 +5,7 @@
  */
 package com.bounswe.group7.repository;
 
-import com.bounswe.group7.model.Quizes;
+import com.bounswe.group7.model.Comments;
 import java.io.Serializable;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
@@ -14,8 +14,9 @@ import org.springframework.data.repository.CrudRepository;
  *
  * @author myunu
  */
-public interface QuizesRepository extends CrudRepository<Quizes, Long>{
+public interface CommentsRepository extends CrudRepository<Comments, Long>{
     
-    public List<Quizes> findByUserId(Long userId);
+    public List<Comments> findByUserId(Long userId);
     
+    public List<Comments> findByTopicId(Long topicId);
 }
