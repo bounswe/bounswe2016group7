ce39113d632432a64a19cddad8daaa00d65ac326
Batuhan Tuna <batuhan.tuna@boun.edu.tr>
2016-11-22 22:40:58 +0300

    TopicWithStatistics Rate Added

--- utopic/web-application/src/main/java/com/bounswe/group7/web/domain/TopicWithStatistics.java
+++ utopic/web-application/src/main/java/com/bounswe/group7/web/domain/TopicWithStatistics.java
@@ -13,10 +13,12 @@ public class TopicWithStatistics {
     public Long topic_id;
     public String topic_name;
     public int commentNumber;
+    public Double rate;
 
-    public TopicWithStatistics(Long topic_id, String topic_name, int commentNumber) {
+    public TopicWithStatistics(Long topic_id, String topic_name, int commentNumber, Double rate) {
         this.topic_id = topic_id;
         this.topic_name = topic_name;
         this.commentNumber = commentNumber;
+        this.rate = rate;
     }
 }
