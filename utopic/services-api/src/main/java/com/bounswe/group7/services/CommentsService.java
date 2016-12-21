2a631a067d56846a62357334c7c987895a3508df
myunusseker <m.yunusseker@hotmail.com>
2016-12-21 16:57:44 +0300

    Comment Servive Documentation

--- utopic/services-api/src/main/java/com/bounswe/group7/services/CommentsService.java
+++ utopic/services-api/src/main/java/com/bounswe/group7/services/CommentsService.java
@@ -15,8 +15,8 @@ import com.bounswe.group7.repository.CommentsRepository;
 import com.bounswe.group7.repository.VotedCommentsRepository;
 
 /**
- *
- * @author myunu
+ * This class is the service class of the Comments Class.
+ * @author Yunus Seker
  */
 @Service
 public class CommentsService {
@@ -29,7 +29,13 @@ public class CommentsService {
     
     @Autowired
     VotedCommentsRepository votedCommentsRepository;
-    
+    /**
+     * This method takes a comment object and 
+     * creates a comment with the logged in user,
+     * sets its rate to zero to initialize it.
+     * @param comment object that has text in it.
+     * @return returns the added comment object
+     */
     public Comments createComment(Comments comment)
     {
         comment.setUserId(usersService.getLoggedInUserId());
@@ -38,11 +44,22 @@ public class CommentsService {
         return commentsRepository.save(comment);
     }
     
+    /**
+     * This method takes a topic id and returns its comments.
+     * @param topicId The related topic's id.
+     * @return returns a Comment List of the topic.
+     */
     public List<Comments> getTopicComments(Long topicId)
     {
         return commentsRepository.findByTopicIdOrderByDateCreatedDesc(topicId);
     }
     
+    /**
+     * This method takes a comment id and finds it in the database.
+     * Deletes it.
+     * @param commentId id of the comment to be deleted
+     * @return returns true if it could delete. other wise returns false.
+     */
     public boolean deleteComment(Long commentId)
     {
         if(commentsRepository.exists(commentId))
@@ -51,6 +68,14 @@ public class CommentsService {
         return true;
     }
     
+    /**
+     * This method takes a VotedComments object and looks to the database.
+     * If user voted that comment previously and the vote is same, it fails to vote.
+     * Otherwise it changes the vote to new one and re-calculates its rate.
+     * If user never voted previously, it adds the new vote to database.
+     * @param votedComment the object that has comment id and vote(1 or -1).
+     * @return returns true if the user can vote. Otherwise false.
+     */
     public boolean voteComment(VotedComments votedComment)
     {
         VotedComments temp = votedCommentsRepository.findByUserIdAndCommentId(usersService.getLoggedInUserId(), votedComment.getCommentId());
@@ -72,6 +97,11 @@ public class CommentsService {
             return true;
         }
     }
+    /**
+     * This method returns the rate of the given comment
+     * @param commentId Id of the comment which its rate will be given.
+     * @return returns an integer rate of the comment.
+     */
     public int getCommentRate(Long commentId)
     {
         return commentsRepository.findOne(commentId).getRate();
